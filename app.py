b53a595e422dd6b81bf0d647b77886cad229a026
louisschild <louisschild@riseup.net>
2025-11-16 09:44:07 +0100

    update

--- app.py
+++ app.py
@@ -1,12 +1,2 @@
-from flask import flask
-
-app = Flask(_name_)
-
-
-@app.route("/")
-def home():
-    return "Hello depuis Flask sur Render!"
-
-if _name_ == "_main_":
-    app.run(host="0.0.0.0", port=50000)
-
+if 5 > 2:
+    print ("Five is greater than two")
