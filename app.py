113a53fa39e2e9e8ce818cc22989c4422abe280b
louisschild <louisschild@riseup.net>
2025-11-16 09:59:27 +0100

    updategit

--- app.py
+++ app.py
@@ -1,26 +1,7 @@
 from flask import flask
-
-
-
 app = Flask(_name_)
-
-
-
-
-
 @app.route("/")
-
-
 def home():
-
-
     return "Hello depuis Flask sur Render!"
-
-
-
-
-
 if _name_ == "_main_":
-
-
     app.run(host="0.0.0.0", port=50000)
